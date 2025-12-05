036294b46ef5ff3674ffb4e930ce71d473bdda65
Harmans <harmans6282@gmail.com>
2025-12-05 20:31:29 +0530

    nativewind setup

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,18 +1,22 @@
 import { Slot, Stack } from "expo-router";
 import { ThemeProvider, DarkTheme } from "@react-navigation/native";
+// @ts-ignore
+import "../../global.css";
+import { StatusBar } from "react-native";
 
 const myTheme = {
   ...DarkTheme,
   colors: {
     ...DarkTheme.colors,
-    primary: 'white',
-    card: '#101010'
+    primary: "white",
+    card: "#101010",
   },
 };
 
 export default function RootLayout() {
   return (
     <ThemeProvider value={myTheme}>
+      <StatusBar barStyle="light-content" />
       <Slot />
     </ThemeProvider>
   );
