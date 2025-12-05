52a8598d07cf17717fa986747a6a2ce578531e9e
Harmans <harmans6282@gmail.com>
2025-12-05 12:11:32 +0530

    expo bottom tabs

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Mobile apps</Text>
+      <Text>Feed</Text>
       <StatusBar style="auto" />
     </View>
   );
